1587ef547e119d71b64abbe9e656a04464f71f19
Algonouir <algo.mahdi.nouri@gmail.com>
2025-11-07 08:59:10 +0330

    add status code after responses

--- src/web/login.tsx
+++ src/web/login.tsx
@@ -3,9 +3,11 @@ import { convertFromSchema, type structure } from "minimal-form";
 import EasyForm from "minimal-form";
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useAuth } from "../hooks/useAuth";
 
 interface ScenarioStepsType {
-  [key: string]: structure;
+  name: string;
+  structure: structure;
 }
 
 interface LoginFormProps {
@@ -13,10 +15,13 @@ interface LoginFormProps {
 }
 
 export default function LoginForm({ baseURL }: LoginFormProps) {
-  const [steps, stepsHnadler] = useState<ScenarioStepsType>({});
-  const [activeStep, activeStepHandler] = useState("");
+  const [steps, stepsHnadler] = useState<ScenarioStepsType[]>();
+  const [activeStep, activeStepHandler] = useState<
+    ScenarioStepsType | undefined
+  >();
   const [loading, loadingHandler] = useState(true);
   const { handleSubmit, control } = useForm();
+  const { fallback_401_url } = useAuth();
 
   const request = axios.create({
     baseURL,
@@ -26,18 +31,35 @@ export default function LoginForm({ baseURL }: LoginFormProps) {
   useEffect(() => {
     async function fetchSteps() {
       const response = await request.get("/options");
-      const data = Object.entries(response.data).map(([opt, schema]) => [
-        opt,
-        convertFromSchema(schema),
-      ]);
-      activeStepHandler(data[0][0] as unknown as string);
-      stepsHnadler(Object.fromEntries(data));
+      const data = Object.entries(response.data).map(([opt, schema]) => ({
+        name: opt,
+        structure: convertFromSchema(schema),
+      }));
+      activeStepHandler(data[0]);
+      stepsHnadler(data);
       loadingHandler(false);
     }
     fetchSteps();
   }, []);
 
-  const onSubmit = (data: any) => {};
+  const onSubmit = async (data: any) => {
+    const response = await request.post(activeStep!.name, data);
+
+    // if response say go next step
+    if (response.status === 202) {
+      const nextIndex =
+        (steps?.findIndex((d) => d.name === activeStep?.name) || 0) + 1;
+      activeStepHandler(steps?.[nextIndex]);
+    }
+    // if response say authenticate is finished
+    else if (response.status === 200) {
+      window.location.href = fallback_401_url;
+    }
+    // if response say have a wrong in this request
+    else if (response.status === 400) {
+      activeStepHandler(steps?.[0]);
+    }
+  };
 
   // loading check
   if (loading) return <p>loading ...</p>;
@@ -45,7 +67,7 @@ export default function LoginForm({ baseURL }: LoginFormProps) {
   // show forms from steps
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <EasyForm control={control} structure={steps[activeStep]} />
+      <EasyForm control={control} structure={activeStep!.structure} />
       <button type="submit">Submit</button>
     </form>
   );
